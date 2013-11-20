a381bc30021113eb306753b5312e4bb299b99f44
oscar-raig <oscar.raig@gmail.com>
2013-11-20 22:00:02 +0100

    Adding CTrace files

---
+++ CTrace.cc
@@ -0,0 +1,8 @@
+
+#include "CTrace.hpp"
+
+void CFuncTrace::trace(char *szToPrint)
+{
+    cout << szToPrint << endl;
+
+}
---
+++ CTrace.hpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+
+using namespace std;
+
+
+
+class CTrace
+{
+
+public:
+    
+    
+    
+};
+
+
+class CFuncTrace
+{
+    
+public:
+    CFuncTrace (){};
+    void trace(char *szToTrace);
+    
+};

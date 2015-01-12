ef3a327a7f78fd0e7d89a1629d9547a7e6a92e46
oscar-raig <oscar.raig@gmail.com>
2015-01-12 20:00:10 +0100

    CTrace fixing

--- CTrace.cpp
+++ CTrace.cpp
@@ -2,12 +2,32 @@
 #include "CTrace.hpp"
 
 
-void CFuncTrace::trace(const char *szFormat, ...){
-	va_list vaArguments;
-	va_start(vaArguments, szFormat);
+void CFuncTrace::trace(const char *Format, va_list  vaArguments){
 	char szOutput[10000];
-	vsnprintf(szOutput, sizeof(szOutput), szFormat, vaArguments);
-	cout << m_szFunctionName + " "  + szOutput << endl;
-	va_end(vaArguments);
+	vsnprintf(szOutput, 1000, Format, vaArguments);
+	cout << this->FunctionName + " "  + szOutput << endl;
 }
 
+
+void CFuncTrace::trace(int level, const char *Format, ...){
+	if ( level < levelTrace)
+		return;
+	
+	va_list vaArguments;
+	va_start(vaArguments, Format);
+//	trace(Format,vaArguments);
+	char szOutput[10000];
+	vsnprintf(szOutput, 1000, Format, vaArguments);
+	string traceLevelName = "DEBUG";
+	switch ( level ){
+		case CTrace::TRACE_ERROR:
+			traceLevelName = "ERROR";
+			break;
+		case CTrace::TRACE_INFO:
+			traceLevelName = "INFO";
+			break;
+	}
+	cout << traceLevelName << " " << this->FunctionName + " "  + szOutput << endl;
+	va_end(vaArguments);
+
+}
--- CTrace.hpp
+++ CTrace.hpp
@@ -1,6 +1,8 @@
 
 #include <iostream>
 
+#ifndef CTRACE_H
+#define CTRACE_H
 
 using namespace std;
 
@@ -9,24 +11,42 @@ using namespace std;
 class CTrace{
 
 public:
-    
-    
-    
+	
+	enum level
+	{
+		TRACE_DEBUG,
+		TRACE_INFO,
+		TRACE_ERROR
+	};
+	
+	
 };
 
 
 class CFuncTrace{
 public:
-	std::string m_szFunctionName;
+	int levelTrace;
+	std::string FunctionName;
+	bool traceOnCreateAndDestroy;
 public:
-    CFuncTrace (string szFunctionName){
-		m_szFunctionName = szFunctionName;
-		cout << m_szFunctionName + ">>" << endl;
+    CFuncTrace (bool traceOnCreateAndDestroy,string FunctionName){
+
+		levelTrace = CTrace::level::TRACE_INFO;
+//		levelTrace = CTrace::level::TRACE_DEBUG;
+//		levelTrace = CTrace::level::TRACE_ERROR;
+		
+		this->FunctionName = FunctionName;
+		this->traceOnCreateAndDestroy = traceOnCreateAndDestroy;
+		if (traceOnCreateAndDestroy)
+			cout << FunctionName + ">>" << endl;
 	};
 	
 	~CFuncTrace(){
-		cout << m_szFunctionName + "<<" << endl;
+		if (this->traceOnCreateAndDestroy)
+			cout << FunctionName + "<<" << endl;
 	}
-	void trace(const char *szFormat, ...);
+	void trace( const char *Format,va_list  vaArguments);
+	void trace(int level, const char *Format, ...);
     
 };
+#endif
